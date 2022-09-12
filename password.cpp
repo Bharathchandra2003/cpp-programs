44b8b676f72b20276784df10f44b9f8de6d0314e
Bharath chandra <112483500+Bharathchandra2003@users.noreply.github.com>
2022-09-12 10:14:47 +0530

    Add files via upload

---
+++ password.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	string a,b;
+	cout<<"enter the password:";
+	cin>>a;
+	cout<<"re-enter the number:";
+	cin>>b;
+	if(a==b)
+	{
+		cout<<"the password is correct";
+	}
+	else
+	{
+		cout<<"incorrect password";
+	}
+	}
+	
+	
+	
+
